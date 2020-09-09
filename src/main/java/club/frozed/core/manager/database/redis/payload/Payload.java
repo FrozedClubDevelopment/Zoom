188f0df9e159dd8d9b3c963ba8744bdf2f2aca4e
Alex Avila <alex.ramon2004@gmail.com>
2020-09-08 22:43:28 -0500

    Payload Redis

---
+++ src/main/java/club/frozed/core/manager/database/redis/payload/Payload.java
@@ -0,0 +1,17 @@
+package club.frozed.core.manager.database.redis.payload;
+
+/**
+ * Created by Ryzeon
+ * Project: Zoom
+ * Date: 8/09/2020 @ 18:43
+ * Template by Elp1to
+ */
+
+public enum  Payload {
+    SERVER_MANAGER,
+    ADMIN_CHAT,
+    STAFF_CHAT,
+    STAFF_JOIN,
+    STAFF_LEAVE,
+    STAFF_SWITCH
+}
