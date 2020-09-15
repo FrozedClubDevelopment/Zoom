79153833837502e268d747da100e43bd169135ec
Alex Avila <alex.ramon2004@gmail.com>
2020-09-14 22:19:23 -0500

    Rank

---
+++ src/main/java/club/frozed/core/manager/ranks/Rank.java
@@ -0,0 +1,36 @@
+package club.frozed.core.manager.ranks;
+
+import club.frozed.core.Zoom;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.ChatColor;
+
+import java.util.List;
+
+/**
+ * Created by Ryzeon
+ * Project: Zoom
+ * Date: 14/09/2020 @ 20:44
+ * Template by Elp1to
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Rank {
+
+    private String name, prefix, suffix;
+    private ChatColor color;
+    private int priority;
+    private boolean defaultRank, bold, italic;
+    private List<String> permissions;
+
+    public Rank() {
+
+    }
+
+    public static Rank getRankByName(String name){
+        return Zoom.getInstance().getRankManager().getRanks().stream().filter(rank -> rank.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+    }
+}
