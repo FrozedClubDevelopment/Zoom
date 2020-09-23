6f3e5cbd07f21c0b4c8215afa0ceafb2f3319e65
Alex Avila <alex.ramon2004@gmail.com>
2020-09-23 12:47:50 -0500

    Grant Menu

--- src/main/java/club/frozed/core/menu/grant/GrantMenu.java
+++ src/main/java/club/frozed/core/menu/grant/GrantMenu.java
@@ -1,10 +1,76 @@
 package club.frozed.core.menu.grant;
 
+import club.frozed.core.Zoom;
+import club.frozed.core.manager.player.PlayerData;
+import club.frozed.core.utils.CC;
+import club.frozed.core.utils.InventoryUtil;
+import club.frozed.core.utils.menu.Menu;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
 /**
  * Created by Ryzeon
  * Project: Zoom
  * Date: 23/09/2020 @ 12:12
  */
 
-public class GrantMenu {
+public class GrantMenu implements Menu {
+    private Inventory inventory;
+
+    public GrantMenu() {
+        this.inventory = Bukkit.createInventory((InventoryHolder) this, 9 * 5, CC.translate(Zoom.getInstance().getSettingsConfig().getConfig().getString("chat.chat-color-menu")));
+    }
+
+    @Override
+    public void open(Player player) {
+        this.update(player);
+        player.openInventory(this.inventory);
+    }
+
+    public void update(Player player) {
+        this.inventory.clear();
+        InventoryUtil.fillInventory(this.inventory);
+    }
+
+    @Override
+    public void onInventoryClick(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+        PlayerData data = PlayerData.getByUuid(p.getUniqueId());
+        final Inventory clickedInventory = e.getClickedInventory();
+        final Inventory topInventory = e.getView().getTopInventory();
+        if (!topInventory.equals(this.inventory)) {
+            return;
+        }
+        if (topInventory.equals(clickedInventory)) {
+            e.setCancelled(true);
+            if (e.getCurrentItem() == null || e.getCurrentItem().getType().equals(Material.AIR) || e.getCurrentItem().getType().equals(Material.STAINED_GLASS_PANE))
+                return;
+            if (!e.getCurrentItem().hasItemMeta()) return;
+            int slots = e.getSlot();
+            switch (slots) {
+                default:
+                    break;
+            }
+        } else if ((!topInventory.equals(clickedInventory) && e.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) || e.getAction() == InventoryAction.COLLECT_TO_CURSOR) {
+            e.setCancelled(true);
+        }
+    }
+
+    public void playSound(Player player, boolean confirmation) {
+        if (confirmation) {
+            player.playSound(player.getLocation(), Sound.NOTE_PLING, 2F, 2F);
+        } else {
+            player.playSound(player.getLocation(), Sound.ITEM_BREAK, 2F, 2F);
+        }
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
 }
