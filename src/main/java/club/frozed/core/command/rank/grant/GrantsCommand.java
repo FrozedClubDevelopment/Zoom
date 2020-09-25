f850b234c6d10cb990fdcb5d4ee0571e9c37ba18
Alex Avila <alex.ramon2004@gmail.com>
2020-09-25 14:44:11 -0500

    Grants lore

---
+++ src/main/java/club/frozed/core/command/rank/grant/GrantsCommand.java
@@ -0,0 +1,10 @@
+package club.frozed.core.command.rank.grant;
+
+/**
+ * Created by Ryzeon
+ * Project: Zoom
+ * Date: 24/09/2020 @ 21:29
+ */
+
+public class GrantsCommand {
+}
---
+++ src/main/java/club/frozed/core/menu/grant/AllGrantsMenu.java
@@ -0,0 +1,10 @@
+package club.frozed.core.menu.grant;
+
+/**
+ * Created by Ryzeon
+ * Project: Zoom
+ * Date: 25/09/2020 @ 14:27
+ */
+
+public class AllGrantsMenu {
+}
