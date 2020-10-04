8a23c231f7d8da8f865ef06710e319dd6cad64e8
Victor Abrego <elbitomc@gmail.com>
2020-10-04 18:50:03 -0500

    Added new menu API

---
+++ src/main/java/club/frozed/core/utils/gui/pagination/PageButton.java
@@ -0,0 +1,61 @@
+package club.frozed.core.utils.gui.pagination;
+
+import club.frozed.core.utils.CC;
+import club.frozed.core.utils.gui.Button;
+import lombok.AllArgsConstructor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+public class PageButton extends Button {
+
+    private int mod;
+    private PaginatedMenu menu;
+
+    @Override
+    public ItemStack getButtonItem(Player player) {
+        ItemStack itemStack = new ItemStack(Material.CARPET);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        if (this.hasNext(player)) {
+            itemMeta.setDisplayName(this.mod > 0 ? CC.translate("&aNext page") : CC.translate("&cPrevious page"));
+        } else {
+            itemMeta.setDisplayName(CC.translate("&7") + (this.mod > 0 ? "Last page" : "First page"));
+        }
+
+        itemMeta.setLore(Arrays.asList(
+                "",
+                CC.translate("&cRight click to"),
+                CC.translate("&cjump to a page")
+        ));
+
+        itemStack.setItemMeta(itemMeta);
+
+        return itemStack;
+    }
+
+    @Override
+    public void clicked(Player player, int i, ClickType clickType, int hb) {
+        if (clickType == ClickType.RIGHT) {
+            new ViewAllPagesMenu(this.menu).openMenu(player);
+            playNeutral(player);
+        } else {
+            if (hasNext(player)) {
+                this.menu.modPage(player, this.mod);
+                Button.playNeutral(player);
+            } else {
+                Button.playFail(player);
+            }
+        }
+    }
+
+    private boolean hasNext(Player player) {
+        int pg = this.menu.getPage() + this.mod;
+        return pg > 0 && this.menu.getPages(player) >= pg;
+    }
+}
---
+++ src/main/java/club/frozed/core/utils/gui/pagination/PaginatedMenu.java
@@ -0,0 +1,80 @@
+package club.frozed.core.utils.gui.pagination;
+
+import club.frozed.core.utils.gui.Button;
+import club.frozed.core.utils.gui.Menu;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class PaginatedMenu extends Menu {
+
+    @Getter
+    private int page = 1;
+
+    { setUpdateAfterClick(false); }
+
+    @Override
+    public String getTitle(Player player) {
+        return getPrePaginatedTitle(player) + " - " + page + "/" + getPages(player);
+    }
+
+    public final void modPage(Player player, int mod) {
+        page += mod;
+        getButtons().clear();
+        openMenu(player);
+    }
+
+    public final int getPages(Player player) {
+        int buttonAmount = getAllPagesButtons(player).size();
+
+        if (buttonAmount == 0) {
+            return 1;
+        }
+
+        return (int) Math.ceil(buttonAmount / (double) getMaxItemsPerPage(player));
+    }
+
+    @Override
+    public final Map<Integer, Button> getButtons(Player player) {
+        int minIndex = (int) ((double) (page - 1) * getMaxItemsPerPage(player));
+        int maxIndex = (int) ((double) (page) * getMaxItemsPerPage(player));
+
+        HashMap<Integer, Button> buttons = new HashMap<>();
+
+        buttons.put(0, new PageButton(-1, this));
+        buttons.put(8, new PageButton(1, this));
+
+        for (Map.Entry<Integer, Button> entry : getAllPagesButtons(player).entrySet()) {
+            int ind = entry.getKey();
+
+            if (ind >= minIndex && ind < maxIndex) {
+                ind -= (int) ((double) (getMaxItemsPerPage(player)) * (page - 1)) - 9;
+                buttons.put(ind, entry.getValue());
+            }
+        }
+
+        Map<Integer, Button> global = getGlobalButtons(player);
+
+        if (global != null) {
+            for (Map.Entry<Integer, Button> gent : global.entrySet()) {
+                buttons.put(gent.getKey(), gent.getValue());
+            }
+        }
+
+        return buttons;
+    }
+
+    public int getMaxItemsPerPage(Player player) {
+        return 18;
+    }
+
+    public Map<Integer, Button> getGlobalButtons(Player player) {
+        return null;
+    }
+
+    public abstract String getPrePaginatedTitle(Player player);
+
+    public abstract Map<Integer, Button> getAllPagesButtons(Player player);
+}
