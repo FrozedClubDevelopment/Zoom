f29ec14c96bbec348c9f2f431772e5bf98a07e4a
Victor Abrego <elbitomc@gmail.com>
2020-10-04 19:16:20 -0500

    Fix y mejor formato

--- src/main/java/club/frozed/core/command/network/PlayerInfoCommand.java
+++ src/main/java/club/frozed/core/command/network/PlayerInfoCommand.java
@@ -20,7 +20,7 @@ public class PlayerInfoCommand extends BaseCMD {
         Player player = command.getPlayer();
         String[] args = command.getArgs();
 
-        if (!(args.length > 1)) {
+        if (args.length < 1) {
             player.sendMessage(CC.translate("&c" + command.getCommand().getUsage()));
             return;
         }
@@ -36,8 +36,8 @@ public class PlayerInfoCommand extends BaseCMD {
 
         Clickable showPermsList = new Clickable();
         List<String> playerPerms = new ArrayList<>();
-        playerData.getPermissions().forEach(perm -> playerPerms.add(CC.translate("&b" + perm)));
-        showPermsList.add(CC.translate("&8 ► &7Permissions&f: &e" + playerData.getPermissions().size()), StringUtils.join(playerPerms, "\n"), null);
+        playerData.getPermissions().forEach(perm -> playerPerms.add(CC.translate("&f ● &6" + perm)));
+        showPermsList.add(CC.translate("&8 ► &7Permissions&f: &e" + playerData.getPermissions().size() + "&8(&7Hover&8)"), StringUtils.join(playerPerms, "\n"), null);
         showPermsList.sendToPlayer(player);
 
         player.sendMessage(CC.translate("&8 ► &7Last Server&f: &e" + playerData.getLastServer()));
