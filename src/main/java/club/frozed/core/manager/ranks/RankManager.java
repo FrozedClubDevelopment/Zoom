545aefb11e1fe620e34c83506ec051b79cd7d9a6
Elb1to <elbitomc@gmail.com>
2020-10-10 12:43:35 -0500

    Reformatted shit

--- src/main/java/club/frozed/core/manager/ranks/RankManager.java
+++ src/main/java/club/frozed/core/manager/ranks/RankManager.java
@@ -8,7 +8,6 @@ import club.frozed.core.manager.player.grants.Grant;
 import club.frozed.core.utils.CC;
 import club.frozed.core.utils.TaskUtil;
 import club.frozed.core.utils.config.FileConfig;
-import club.frozed.core.utils.grant.GrantUtil;
 import club.frozed.core.utils.lang.Lang;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.UpdateOptions;
@@ -29,43 +28,41 @@ import java.util.stream.Collectors;
  * Date: 14/09/2020 @ 20:44
  * Template by Elp1to
  */
-
 public class RankManager {
 
     public void loadRanks() {
-        if (Zoom.getInstance().getMongoManager().getRanksData().find().into(new ArrayList<>()).isEmpty()){
+        if (Zoom.getInstance().getMongoManager().getRanksData().find().into(new ArrayList<>()).isEmpty()) {
             loadRanksFromConfig();
-        }
-        else {
+        } else {
             loadRanksFromMongo();
         }
     }
 
     public void loadRanksFromConfig() {
-            Rank.ranks.clear();
-            try {
-                for (String rank : Zoom.getInstance().getRanksConfig().getConfig().getKeys(false)) {
-                    String rankName = Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".NAME");
-                    String rankPrefix = Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".PREFIX");
-                    String rankSuffix = Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".SUFFIX");
-                    ChatColor rankColor = ChatColor.valueOf(Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".COLOR"));
-                    if (rankColor == null){
-                        rankColor = ChatColor.WHITE;
-                    }
-                    boolean rankDefault = Zoom.getInstance().getRanksConfig().getConfig().getBoolean(rank + ".DEFAULT");
-                    boolean rankBold = Zoom.getInstance().getRanksConfig().getConfig().getBoolean(rank + ".BOLD");
-                    boolean rankItalic = Zoom.getInstance().getRanksConfig().getConfig().getBoolean(rank + ".ITALIC");
-                    int rankPriority = Zoom.getInstance().getRanksConfig().getConfig().getInt(rank + ".PRIORITY");
-                    List<String> rankInheritance = Zoom.getInstance().getRanksConfig().getConfig().getStringList(rank + ".INHERITANCE");
-                    List<String> rankPermission = Zoom.getInstance().getRanksConfig().getConfig().getStringList(rank + ".PERMISSIONS");
-
-                    new Rank(rankName, rankPrefix, rankSuffix, rankColor, rankPriority, rankDefault, rankBold, rankItalic, rankPermission, rankInheritance);
+        Rank.ranks.clear();
+        try {
+            for (String rank : Zoom.getInstance().getRanksConfig().getConfig().getKeys(false)) {
+                String rankName = Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".NAME");
+                String rankPrefix = Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".PREFIX");
+                String rankSuffix = Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".SUFFIX");
+                ChatColor rankColor = ChatColor.valueOf(Zoom.getInstance().getRanksConfig().getConfig().getString(rank + ".COLOR"));
+                if (rankColor == null) {
+                    rankColor = ChatColor.WHITE;
                 }
-                Bukkit.getConsoleSender().sendMessage(Lang.PREFIX + "§eSuccessfully loaded §f" + Rank.ranks.size() + " §eranks from Ranks.yml");
-            } catch (Exception e) {
-                Bukkit.getConsoleSender().sendMessage(Lang.PREFIX + "§cAn error occurred while loading the ranks. Please check your config!");
-                Bukkit.shutdown();
+                boolean rankDefault = Zoom.getInstance().getRanksConfig().getConfig().getBoolean(rank + ".DEFAULT");
+                boolean rankBold = Zoom.getInstance().getRanksConfig().getConfig().getBoolean(rank + ".BOLD");
+                boolean rankItalic = Zoom.getInstance().getRanksConfig().getConfig().getBoolean(rank + ".ITALIC");
+                int rankPriority = Zoom.getInstance().getRanksConfig().getConfig().getInt(rank + ".PRIORITY");
+                List<String> rankInheritance = Zoom.getInstance().getRanksConfig().getConfig().getStringList(rank + ".INHERITANCE");
+                List<String> rankPermission = Zoom.getInstance().getRanksConfig().getConfig().getStringList(rank + ".PERMISSIONS");
+
+                new Rank(rankName, rankPrefix, rankSuffix, rankColor, rankPriority, rankDefault, rankBold, rankItalic, rankPermission, rankInheritance);
             }
+            Bukkit.getConsoleSender().sendMessage(Lang.PREFIX + "§eSuccessfully loaded §f" + Rank.ranks.size() + " §eranks from Ranks.yml");
+        } catch (Exception e) {
+            Bukkit.getConsoleSender().sendMessage(Lang.PREFIX + "§cAn error occurred while loading the ranks. Please check your config!");
+            Bukkit.shutdown();
+        }
     }
 
     public void saveRanks() {
@@ -91,7 +88,7 @@ public class RankManager {
         }
     }
 
-    public void loadRanksFromMongo(){
+    public void loadRanksFromMongo() {
         Rank.ranks.clear();
         try {
             List<Document> documents = Zoom.getInstance().getMongoManager().getRanksData().find().into(new ArrayList<>());
@@ -119,20 +116,21 @@ public class RankManager {
         }
     }
 
-    public void saveFromMongo(){
+    public void saveFromMongo() {
         List<Document> documents = Zoom.getInstance().getMongoManager().getRanksData().find().into(new ArrayList<>());
-        for (Document document : documents){
+        for (Document document : documents) {
             String rankName = document.getString("NAME");
             String rankPrefix = document.getString("PREFIX");
             String rankSuffix = document.getString("SUFFIX");
             ChatColor rankColor = ChatColor.valueOf(document.getString("COLOR"));
-            if (rankColor == null){
+            if (rankColor == null) {
                 rankColor = ChatColor.WHITE;
             }
             boolean rankDefault = document.getBoolean("DEFAULT");
             boolean rankBold = document.getBoolean("BOLD");
             boolean rankItalic = document.getBoolean("ITALIC");
-            int rankPriority = document.getInteger("PRIORITY");;
+            int rankPriority = document.getInteger("PRIORITY");
+            ;
             List<String> rankPermission = (List<String>) document.get("PERMISSIONS");
             List<String> rankInheritance = (List<String>) document.get("INHERITANCE");
 
@@ -161,17 +159,17 @@ public class RankManager {
         return defaults.get(0);
     }
 
-    public void updateRank(Rank rank){
-        if (rank != null){
+    public void updateRank(Rank rank) {
+        if (rank != null) {
             rank.update();
         }
-        String json = new RedisMessage(Payload.RANK_UPDATE_PERMS).setParam("RANK",rank.getName()).toJSON();
+        String json = new RedisMessage(Payload.RANK_UPDATE_PERMS).setParam("RANK", rank.getName()).toJSON();
 
         Zoom.getInstance().getRedisManager().write(json);
     }
 
     public void deleteRank(Rank rank) {
-        String json = new RedisMessage(Payload.RANK_DELETE).setParam("RANK",rank.getName()).toJSON();
+        String json = new RedisMessage(Payload.RANK_DELETE).setParam("RANK", rank.getName()).toJSON();
         Zoom.getInstance().getRedisManager().write(json);
     }
 
@@ -196,36 +194,36 @@ public class RankManager {
             if (grant.isPermanent()) {
                 sender.sendMessage(CC.translate(Lang.PREFIX + "&aSuccess! &7Added permanently grant " + grant.getRank().getName() + " to " + targetData.getName()));
                 json = new RedisMessage(Payload.GRANT_ADD)
-                        .setParam("NAME",targetData.getName())
-                        .setParam("MESSAGE",CC.translate(Zoom.getInstance().getMessagesConfig().getConfig().getString("COMMANDS.GRANT.PERM").replace("<rank>",rankData.getName()))).toJSON();
-                if (Zoom.getInstance().getRedisManager().isActive()){
+                        .setParam("NAME", targetData.getName())
+                        .setParam("MESSAGE", CC.translate(Zoom.getInstance().getMessagesConfig().getConfig().getString("COMMANDS.GRANT.PERM").replace("<rank>", rankData.getName()))).toJSON();
+                if (Zoom.getInstance().getRedisManager().isActive()) {
                     Zoom.getInstance().getRedisManager().write(json);
                 } else {
                     if (targetData.getPlayer() != null && targetData.getPlayer().isOnline()) {
-                        targetData.getPlayer().sendMessage(CC.translate(Zoom.getInstance().getMessagesConfig().getConfig().getString("COMMANDS.GRANT.PERM").replace("<rank>",rankData.getName())));
+                        targetData.getPlayer().sendMessage(CC.translate(Zoom.getInstance().getMessagesConfig().getConfig().getString("COMMANDS.GRANT.PERM").replace("<rank>", rankData.getName())));
                     }
                 }
             } else {
-                sender.sendMessage(CC.translate(Lang.PREFIX + "&aSuccess! &7Added permanently grant " + grant.getRank().getName() + " to " + targetData.getName()  + " for " + grant.getNiceDuration()));
+                sender.sendMessage(CC.translate(Lang.PREFIX + "&aSuccess! &7Added permanently grant " + grant.getRank().getName() + " to " + targetData.getName() + " for " + grant.getNiceDuration()));
                 json = new RedisMessage(Payload.GRANT_ADD)
-                        .setParam("NAME",targetData.getName())
-                        .setParam("MESSAGE",CC.translate(Zoom.getInstance().getMessagesConfig().getConfig().getString("COMMANDS.GRANT.TEMP")
-                                .replace("<time>",grant.getNiceDuration())
-                                .replace("<rank>",rankData.getName()))).toJSON();
-                if (Zoom.getInstance().getRedisManager().isActive()){
+                        .setParam("NAME", targetData.getName())
+                        .setParam("MESSAGE", CC.translate(Zoom.getInstance().getMessagesConfig().getConfig().getString("COMMANDS.GRANT.TEMP")
+                                .replace("<time>", grant.getNiceDuration())
+                                .replace("<rank>", rankData.getName()))).toJSON();
+                if (Zoom.getInstance().getRedisManager().isActive()) {
                     Zoom.getInstance().getRedisManager().write(json);
                 } else {
                     if (targetData.getPlayer() != null && targetData.getPlayer().isOnline()) {
                         targetData.getPlayer().sendMessage(CC.translate(Zoom.getInstance().getMessagesConfig().getConfig().getString("COMMANDS.GRANT.TEMP")
-                                .replace("<time>",grant.getNiceDuration())
-                                .replace("<rank>",rankData.getName())));
+                                .replace("<time>", grant.getNiceDuration())
+                                .replace("<rank>", rankData.getName())));
                     }
                 }
             }
             Player target = Bukkit.getPlayer(targetData.getName());
             if (target == null) {
                 json = new RedisMessage(Payload.GRANT_UPDATE)
-                        .setParam("NAME",targetData.getName())
+                        .setParam("NAME", targetData.getName())
                         .setParam("GRANT", grant.getRank().getName()
                                 + ";" + grant.getAddedDate()
                                 + ";" + grant.getDuration()
@@ -236,7 +234,7 @@ public class RankManager {
                                 + ";" + grant.isActive()
                                 + ";" + grant.isPermanent()
                                 + ";" + grant.getServer()).toJSON();
-                if (Zoom.getInstance().getRedisManager().isActive()){
+                if (Zoom.getInstance().getRedisManager().isActive()) {
                     Zoom.getInstance().getRedisManager().write(json);
                 }
             } else {
