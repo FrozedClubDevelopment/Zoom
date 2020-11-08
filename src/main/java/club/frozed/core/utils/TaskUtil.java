9602d7987eb8fc3e5ae812c47a16cbc7dc7fcc00
Elb1to <elbitomc@gmail.com>
2020-11-08 14:18:25 -0500

    Update TaskUtil.java

--- src/main/java/club/frozed/core/utils/TaskUtil.java
+++ src/main/java/club/frozed/core/utils/TaskUtil.java
@@ -1,39 +1,39 @@
 package club.frozed.core.utils;
 
 import club.frozed.core.Zoom;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class TaskUtil {
+
     public static void run(Runnable runnable) {
-        Zoom.getInstance().getServer().getScheduler().runTask((Plugin) Zoom.getInstance(), runnable);
+        Zoom.getInstance().getServer().getScheduler().runTask(Zoom.getInstance(), runnable);
     }
 
     public static void runTimer(Runnable runnable, long delay, long timer) {
-        Zoom.getInstance().getServer().getScheduler().runTaskTimer((Plugin) Zoom.getInstance(), runnable, delay, timer);
+        Zoom.getInstance().getServer().getScheduler().runTaskTimer(Zoom.getInstance(), runnable, delay, timer);
     }
 
     public static void runTimer(BukkitRunnable runnable, long delay, long timer) {
-        runnable.runTaskTimer((Plugin) Zoom.getInstance(), delay, timer);
+        runnable.runTaskTimer(Zoom.getInstance(), delay, timer);
     }
 
     public static void runTimerAsync(BukkitRunnable runnable, long delay, long timer) {
-        runnable.runTaskTimerAsynchronously((Plugin) Zoom.getInstance(), delay, timer);
+        runnable.runTaskTimerAsynchronously(Zoom.getInstance(), delay, timer);
     }
 
     public static void runLater(Runnable runnable, long delay) {
-        Zoom.getInstance().getServer().getScheduler().runTaskLater((Plugin) Zoom.getInstance(), runnable, delay);
+        Zoom.getInstance().getServer().getScheduler().runTaskLater(Zoom.getInstance(), runnable, delay);
     }
 
     public static void runLaterAsync(Runnable runnable, long delay) {
-        Zoom.getInstance().getServer().getScheduler().runTaskLaterAsynchronously((Plugin) Zoom.getInstance(), runnable, delay);
+        Zoom.getInstance().getServer().getScheduler().runTaskLaterAsynchronously(Zoom.getInstance(), runnable, delay);
     }
 
     public static void runTaskTimerAsynchronously(Runnable runnable, int delay) {
-        Zoom.getInstance().getServer().getScheduler().runTaskTimerAsynchronously((Plugin) Zoom.getInstance(), runnable, 20 * delay, 20 * delay);
+        Zoom.getInstance().getServer().getScheduler().runTaskTimerAsynchronously(Zoom.getInstance(), runnable, 20 * delay, 20 * delay);
     }
 
     public static void runAsync(Runnable runnable) {
-        Zoom.getInstance().getServer().getScheduler().runTaskAsynchronously((Plugin) Zoom.getInstance(), runnable);
+        Zoom.getInstance().getServer().getScheduler().runTaskAsynchronously(Zoom.getInstance(), runnable);
     }
 }
