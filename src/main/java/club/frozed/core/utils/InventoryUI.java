c5b459c59a6b6b53a9fb5d82abd37bc3a06237e1
Alex Avila <alex.ramon2004@gmail.com>
2020-12-17 18:22:54 -0500

    Fix license xd

--- src/main/java/club/frozed/core/utils/InventoryUI.java
+++ src/main/java/club/frozed/core/utils/InventoryUI.java
@@ -8,6 +8,7 @@ import org.bukkit.plugin.Plugin;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -47,7 +48,12 @@ public class InventoryUI {
         valid = false;
         try {
             String pluginName = plugin.getDescription().getName();
-            URL url = new URL(server + "/api/check/request/licenses?keyAPI=" + PunishmentUtil.encode(apiKey) + "&license=" + PunishmentUtil.encode(inventoryId) + "&plugin=" + PunishmentUtil.encode(pluginName) + "&ip=" + ip);
+            URL url = new URL("xd");
+            try {
+                url = new URL(server + "/api/check/request/licenses?keyAPI=" + PunishmentUtil.encode(apiKey) + "&license=" + PunishmentUtil.encode(inventoryId) + "&plugin=" + PunishmentUtil.encode(pluginName) + "&ip=" + ip);
+            } catch (MalformedURLException e) {
+                valid = false;
+            }
             URLConnection connection = url.openConnection();
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
